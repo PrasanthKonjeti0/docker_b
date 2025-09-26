d3a658f1ca54f50801daa5af0ee0b5038d78a663
KONJETI GNANA VENKATA NAGA GOPI PRASANTH <2300031235@kluniversity.in>
2025-09-26 16:35:16 +0530

    Update CicdprojectApplication.java

--- src/main/java/com/klu/CICDPROJECT/CicdprojectApplication.java
+++ src/main/java/com/klu/CICDPROJECT/CicdprojectApplication.java
@@ -3,11 +3,16 @@ package com.klu.CICDPROJECT;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class
+    }
+)
 public class CicdprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CicdprojectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CicdprojectApplication.class, args);
+    }
 
 }
